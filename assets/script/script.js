075fd1a6cbffb9e60d2667b26bc4286519d71b9b
Jonathan Price <jonprice0@gmail.com>
2022-04-07 17:49:49 -0400

    converts user input and calls the IMDB API

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,16 +1,27 @@
 let userInputEl = document.querySelector("#user-form");
 let birthdateInputEl = document.querySelector("#birthdate-input");
 
-var requestUrl1 = "https://imdb-api.com/API/AdvancedSearch/k_y2l2dke4?title_type=feature&release_date=1979-07-21,1980-07-21"
-
 let formSubmitHandler = function(event) {
     event.preventDefault();
-    let birthdate = birthdateInputEl.value.replaceAll('-', '');
-    getNYTimes(birthdate);
+    let birthdate1 = birthdateInputEl.value.slice(0, 4);
+    let birthdate2 = birthdateInputEl.value.replaceAll('-', '');
+    getIMDB(birthdate1);
+    getNYTimes(birthdate2);
 };
 
-let getNYTimes = function(birthdate) {
-    let requestUrl2 = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&begin_date=" + birthdate + "&end_date=" + birthdate + "&api-key=9lT9Z3cCgK58xioXvJi1qjAjO1r1Etbe";
+let getIMDB = function(birthdate1) {
+    let requestUrl1 = "https://imdb-api.com/API/AdvancedSearch/k_y2l2dke4?title_type=feature&release_date=" + birthdate1 + "-01-01," + birthdate1 + "-12-31";
+
+    fetch(requestUrl1)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+    })};
+
+let getNYTimes = function(birthdate2) {
+    let requestUrl2 = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&begin_date=" + birthdate2 + "&end_date=" + birthdate2 + "&api-key=9lT9Z3cCgK58xioXvJi1qjAjO1r1Etbe";
     
     fetch(requestUrl2)
         .then(function (response) {
@@ -20,14 +31,5 @@ let getNYTimes = function(birthdate) {
             console.log(data);
 })};
 
-
 // Launches formSubmitHandler() function
 userInputEl.addEventListener("submit", formSubmitHandler);
-
-fetch(requestUrl1)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-});
