5aec4f3ca24b92a23bf5610519e5395c28b6ce33
Dr.Worm <sky.jackson@icloud.com>
2022-04-07 19:17:04 -0400

    fixed randomization

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,18 +1,22 @@
 var year = 1991;
 
 var getUrl = function() {
-    musicUrl = 'https://musicbrainz.org/ws/2/recording/?query=firstreleasedate:' + year + '&country=us&&limit=100&fmt=json'
+    musicUrl = 'https://musicbrainz.org/ws/2/recording/?query=firstreleasedate:' + year + '&country=us&limit=100&fmt=json'
 
     fetch(musicUrl)
         .then(function(response) {
             if (response.ok) {
                 response.json().then(function(data) {
                     var recordings = data.recordings;
-                    // for (var i = 0; i < 10; i++) {
-                        var albums = "";
-                        albums += recordings[Math.floor(Math.random()*recordings.length)];
-                    // }
-                    console.log(albums);
+                    console.log(recordings);
+                    var albums = [];
+                    for (var i = 0; i < 10; i++) {
+                        var album = recordings[Math.floor(Math.random() * recordings.length)]
+                        albums.push(album);
+                        // albums += recordings[Math.floor(Math.random() * recordings.length)];
+                    // console.log(albums);
+                    console.log(albums[i]);
+                    }
                    
                 });
                 
