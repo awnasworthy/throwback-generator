608964b78b059c757d9f8810a798d0c835ea8d24
Jonathan Price <jonprice0@gmail.com>
2022-04-07 17:07:00 -0400

    converts user input and calls the Times API

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,13 +2,24 @@ let userInputEl = document.querySelector("#user-form");
 let birthdateInputEl = document.querySelector("#birthdate-input");
 
 var requestUrl1 = "https://imdb-api.com/API/AdvancedSearch/k_y2l2dke4?title_type=feature&release_date=1979-07-21,1980-07-21"
-var requestUrl2 = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&begin_date=19790721&end_date=19790721&api-key=9lT9Z3cCgK58xioXvJi1qjAjO1r1Etbe"
 
 let formSubmitHandler = function(event) {
     event.preventDefault();
-    let birthdate = birthdateInputEl.value.replaceAll('-', '')
-    console.log(birthdate);
+    let birthdate = birthdateInputEl.value.replaceAll('-', '');
+    getNYTimes(birthdate);
 };
+
+let getNYTimes = function(birthdate) {
+    let requestUrl2 = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&begin_date=" + birthdate + "&end_date=" + birthdate + "&api-key=9lT9Z3cCgK58xioXvJi1qjAjO1r1Etbe";
+    
+    fetch(requestUrl2)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+})};
+
 
 // Launches formSubmitHandler() function
 userInputEl.addEventListener("submit", formSubmitHandler);
@@ -19,11 +30,4 @@ fetch(requestUrl1)
     })
     .then(function (data) {
         console.log(data);
-    });
-fetch(requestUrl2)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    });
+});
