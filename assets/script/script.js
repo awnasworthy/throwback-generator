4c0dbde816bdea3b22e2751d6be58e3bfc93c107
Dr.Worm <sky.jackson@icloud.com>
2022-04-07 12:43:40 -0400

    started random loop to get data from api

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,27 @@
-console.log("hello all")
+var year = 1991;
+
+var getUrl = function() {
+    musicUrl = 'https://musicbrainz.org/ws/2/recording/?query=firstreleasedate:' + year + '&country=us&&limit=100&fmt=json'
+
+    fetch(musicUrl)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    var recordings = data.recordings;
+                    // for (var i = 0; i < 10; i++) {
+                        var albums = "";
+                        albums += recordings[Math.floor(Math.random()*recordings.length)];
+                    // }
+                    console.log(albums);
+                   
+                });
+                
+            }
+        });
+
+
+
+};
+
+
+getUrl();
