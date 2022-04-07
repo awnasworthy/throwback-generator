3d7d74861d59c423a1b6fb2f773ac306da861b46
Jonathan Price <jonprice0@gmail.com>
2022-04-07 16:07:14 -0400

    converts user input for NYTimes API

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,17 @@
+let userInputEl = document.querySelector("#user-form");
+let birthdateInputEl = document.querySelector("#birthdate-input");
 
 var requestUrl1 = "https://imdb-api.com/API/AdvancedSearch/k_y2l2dke4?title_type=feature&release_date=1979-07-21,1980-07-21"
 var requestUrl2 = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&begin_date=19790721&end_date=19790721&api-key=9lT9Z3cCgK58xioXvJi1qjAjO1r1Etbe"
+
+let formSubmitHandler = function(event) {
+    event.preventDefault();
+    let birthdate = birthdateInputEl.value.replaceAll('-', '')
+    console.log(birthdate);
+};
+
+// Launches formSubmitHandler() function
+userInputEl.addEventListener("submit", formSubmitHandler);
 
 fetch(requestUrl1)
     .then(function (response) {
@@ -9,7 +20,6 @@ fetch(requestUrl1)
     .then(function (data) {
         console.log(data);
     });
-
 fetch(requestUrl2)
     .then(function (response) {
         return response.json();
